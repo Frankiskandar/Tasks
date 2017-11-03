f7627744c017543b8a70d56cb11238667d469b68
Fnu Frangky <frank@dc-a9-04-76-1b-e1.wireless.temple.edu>
2017-11-03 15:44:04 -0400

    Initial Commit

---
+++ Frangky_Fnu_Lab04/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Frangky_Fnu_Lab04
+//
+//  Created by Fnu Frangky on 11/3/17.
+//  Copyright © 2017 Fnu Frangky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
